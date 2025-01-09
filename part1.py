e8579debf3f2513680516b1d094b79e917ef1ca1
yural64 <yural64@gmail.com>
2025-01-09 21:19:42 +0300

    add DataFrame

--- part1.py
+++ part1.py
@@ -1,5 +1,10 @@
 import pandas as pd # переименовали в pd для краткости
 
-data = [1, 2, 3, 4, 5]
-series = pd.Series(data)
-print(series)
+data = {
+    'Name' : ['Alice', 'Bob', 'Roma', 'Anna'],
+    'Age' : [23, 45, 17, 24],
+    'City' : ['New York', 'LA', 'Chicago', 'Moscow']
+}
+
+df = pd.DataFrame(data)
+print(df)
