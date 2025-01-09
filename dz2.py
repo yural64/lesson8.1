24f04b689de69c96e5cb9faac42f50076659fa2b
yural64 <yural64@gmail.com>
2025-01-09 23:14:45 +0300

    add code dz2.py

--- dz2.py
+++ dz2.py
@@ -1,6 +1,12 @@
 #Определите среднюю зарплату (Salary) по городу (City) - используйте файл приложенный к дз - dz.csv
-#В поле сдачи домашнего задания приложите ссылку на репозиторий с кодом.
 
 import pandas as pd
 
-df = pd.read_csv('Electricity production by source.csv')
+df = pd.read_csv('dz.csv')
+df.fillna({'Salary': 0}, inplace=True)
+group = df.groupby('City')['Salary'].mean()
+print('Убираем NaN в зарплате:')
+print(df)
+print()
+print('Средняя зарплата по городам:')
+print(group)
