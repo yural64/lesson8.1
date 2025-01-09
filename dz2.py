a9c11ab49d4a0a3212cf70760b16ad13a0050bf2
yural64 <yural64@gmail.com>
2025-01-09 22:56:49 +0300

    Create dz2.py

---
+++ dz2.py
@@ -0,0 +1,6 @@
+#Определите среднюю зарплату (Salary) по городу (City) - используйте файл приложенный к дз - dz.csv
+#В поле сдачи домашнего задания приложите ссылку на репозиторий с кодом.
+
+import pandas as pd
+
+df = pd.read_csv('Electricity production by source.csv')
