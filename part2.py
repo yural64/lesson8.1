9391c7eb29c1276cf911bbd7849270e95076629f
yural64 <yural64@gmail.com>
2025-01-09 22:19:28 +0300

    edit animal.csv

--- part2.py
+++ part2.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+# Работа с некорректными данными
+df = pd.read_csv('animal.csv')
+print(df)
+
+# Заменяем значения NaN на другие (в данном случае на 0)
+#df.fillna(0, inplace=True)
+#print(df)
+
+# Удаляем строки с Nan (не рекомендуется)
+df.dropna(inplace=True)
+print(df)
