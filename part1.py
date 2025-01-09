45fb1fef0eddb895cd94133b0fdff488f3aba412
yural64 <yural64@gmail.com>
2025-01-09 21:23:17 +0300

    clear and add csv

--- part1.py
+++ part1.py
@@ -1,10 +1,3 @@
 import pandas as pd # переименовали в pd для краткости
 
-data = {
-    'Name' : ['Alice', 'Bob', 'Roma', 'Anna'],
-    'Age' : [23, 45, 17, 24],
-    'City' : ['New York', 'LA', 'Chicago', 'Moscow']
-}
-
-df = pd.DataFrame(data)
-print(df)
+df = pd.read_csv('World-happiness-report-2024.csv')
