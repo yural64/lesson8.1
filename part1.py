ea3e4af6859e437ab0ebbbe67104c021578878e7
yural64 <yural64@gmail.com>
2025-01-09 21:49:41 +0300

    add func2

--- part1.py
+++ part1.py
@@ -5,4 +5,8 @@ df = pd.read_csv('World-happiness-report-2024.csv')
 #print(df.tail()) # выводит 5 строк с конца файла
 #print(df.info()) # выводит инфу о дата фрейме
 #print(df.describe()) # выводит статистическое описание данных
-
+#print(df['Country name']) # выводим инфу по конкретному столбцу
+#print(df[['Country name', 'Regional indicator']]) # выводим несколько столбцов
+#print(df.loc[56]) # выводим инфу по строке 56
+#print(df.loc[56, 'Healthy life expectancy']) # вывод значения конкретной ячейки
+#print(df[df['Healthy life expectancy'] > 0.7]) # вывод данных конкретного столбца по условию
