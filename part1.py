7bbe423882e629978449b5d1eb81d736023d5ba9
yural64 <yural64@gmail.com>
2025-01-09 21:34:38 +0300

    add func

--- part1.py
+++ part1.py
@@ -1,3 +1,8 @@
 import pandas as pd # переименовали в pd для краткости
 
 df = pd.read_csv('World-happiness-report-2024.csv')
+#print(df.head()) # выводим первые 5 (по умолчанию) строк из csv
+#print(df.tail()) # выводит 5 строк с конца файла
+#print(df.info()) # выводит инфу о дата фрейме
+#print(df.describe()) # выводит статистическое описание данных
+
