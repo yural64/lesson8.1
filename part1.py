bf3e7df44da26e0ff3f31c5a5f85ed3d6b033342
yural64 <yural64@gmail.com>
2025-01-09 22:07:25 +0300

    add func3

--- part1.py
+++ part1.py
@@ -1,6 +1,7 @@
 import pandas as pd # переименовали в pd для краткости
 
-df = pd.read_csv('World-happiness-report-2024.csv')
+# Изучаем функции pandas
+#df = pd.read_csv('World-happiness-report-2024.csv')
 #print(df.head()) # выводим первые 5 (по умолчанию) строк из csv
 #print(df.tail()) # выводит 5 строк с конца файла
 #print(df.info()) # выводит инфу о дата фрейме
@@ -10,3 +11,16 @@ df = pd.read_csv('World-happiness-report-2024.csv')
 #print(df.loc[56]) # выводим инфу по строке 56
 #print(df.loc[56, 'Healthy life expectancy']) # вывод значения конкретной ячейки
 #print(df[df['Healthy life expectancy'] > 0.7]) # вывод данных конкретного столбца по условию
+
+# Добавляем/удаляем данные
+df = pd.read_csv('hh.csv')
+
+#df['Test'] = [new for new in range(29)] # добавляем столбец Test и заполняем данными через цикл
+#print(df)
+
+#df.drop('Test', axis=1, inplace=True) # удаляем столбец Test, в axis 1/0 - удаляем столбец или строку
+# inplace = True - изменения внести в исходный дата фрейм, False - оригина остается, возвращается измененный дата фрейм
+#print(df)
+
+df.drop(28, axis=0, inplace=True) # удаляем строку 28
+print(df)
